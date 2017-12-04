e1d9cb4bed9f96191b79af1fb35bd5be3f6e709d
Jair Alayon <jair.atl.95@hotmail.com>
2017-12-03 18:08:54 -0600

    Terminacion de la confuguracion de usuario. Cambio de imagen de perfil en firebase

--- app/src/main/java/com/itver/alayon/chatapplicationtest/activitys/SettingsActivity.java
+++ app/src/main/java/com/itver/alayon/chatapplicationtest/activitys/SettingsActivity.java
@@ -1,7 +1,9 @@
 package com.itver.alayon.chatapplicationtest.activitys;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +11,9 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -22,7 +26,10 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.itver.alayon.chatapplicationtest.R;
 import com.itver.alayon.chatapplicationtest.utils.Utils;
+import com.squareup.picasso.Picasso;
 import com.theartofdev.edmodo.cropper.CropImage;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -34,11 +41,14 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
     //UI ELEMENTS
     private TextView textViewUserName;
     private TextView textViewStatus;
+    private CircleImageView circleImageView;
     private Button buttonChangeStatus;
     private Button buttonChangeImage;
+    private ProgressDialog progressDialog;
 
     //DATA FROM DATABASE
     private String uid;
+    private String imageProfile;
     private String userName;
     private String status;
 
@@ -51,6 +61,7 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
 
         textViewUserName = (TextView) findViewById(R.id.textViewUserName);
         textViewStatus = (TextView) findViewById(R.id.textViewStatus);
+        circleImageView = (CircleImageView) findViewById(R.id.profileImage);
         buttonChangeStatus = (Button) findViewById(R.id.buttonChangeStatus);
         buttonChangeImage = (Button) findViewById(R.id.buttonChangePhoto);
 
@@ -67,12 +78,17 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
+                imageProfile = dataSnapshot.child("profile_image").getValue().toString();
                 userName = dataSnapshot.child("user_name").getValue().toString();
                 status = dataSnapshot.child("status").getValue().toString();
 
+                if (!imageProfile.equals("default")) {
+                    Picasso.with(SettingsActivity.this).load(imageProfile).into(circleImageView);
+                }
                 //Volcar sobre los elementos del UI
                 textViewUserName.setText(userName);
                 textViewStatus.setText(status);
+
 
             }
 
@@ -99,7 +115,6 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
                 break;
             case R.id.buttonChangePhoto:
                 //IR AL ACTIVITY PARA CAMBIAR LA FOTO -> Galeria
-
                 Intent gallery = new Intent();
                 gallery.setType("image/*");
                 gallery.setAction(Intent.ACTION_GET_CONTENT);
@@ -117,29 +132,49 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
         if (requestCode == GALLERY_RESULT && resultCode == RESULT_OK) {
             Uri imageUri = data.getData();
-            CropImage.activity(imageUri).start(this);
+            CropImage.activity(imageUri).setAspectRatio(1, 1).start(this);
         }
 
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
 
-                Uri resultUri = result.getUri();
-                //Generar un nombre aleatorio ala imagen y guardarla en la base de datos
-                String nameRandomFile = Utils.getRandomString();
+                progressDialog = new ProgressDialog(SettingsActivity.this);
+                progressDialog.setTitle("Uploading image...");
+                progressDialog.setMessage("Please wait while your image change.");
+                progressDialog.setCanceledOnTouchOutside(false);
+                progressDialog.show();
 
-                StorageReference pathImage = storageReference.child("imagenes_perfil").child(nameRandomFile + ".jpg");
+                Uri resultUri = result.getUri();
+                StorageReference pathImage = storageReference.child("imagenes_perfil").child(uid + ".jpg");
                 pathImage.putFile(resultUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
 
                     @Override
                     public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        Toast.makeText(SettingsActivity.this, "Uploading image...", Toast.LENGTH_SHORT).show();
+
+                        //Si se gurda la imagen -> obtener de descarga y ponersela en el objeto imagen del usuario
+                        String image_path = taskSnapshot.getDownloadUrl().toString();
+                        databaseReference.child("profile_image").setValue(image_path).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+
+                                if (task.isSuccessful()) {
+                                    progressDialog.dismiss();
+                                    Toast.makeText(SettingsActivity.this, "Upload image succesfull", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+
                     }
+
                 });
 
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                progressDialog.dismiss();
                 Exception error = result.getError();
             }
         }
